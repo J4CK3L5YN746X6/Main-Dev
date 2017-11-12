612513e84c2d37c28471689f8b35fcb229d94abf
J4CK3L5YN746X6 <33609280+J4CK3L5YN746X6@users.noreply.github.com>
2017-11-12 23:32:13 +0000

    Added Main Files Version 0.0.3
    Basic Version No Functionality

---
+++ H1V3/h1v3.py
@@ -0,0 +1,28 @@
+import os, time, sys
+# Basic Module import
+# Third Party Modules
+try:
+    import colorama
+    from colorama import Fore, Style, Back
+except:
+    print ' [!] Failed To import colorama module please do "pip install colorama"'
+    sys.exit(1)
+
+class MainOps:
+
+    _Memory_ = {}
+    
+    # _type_ is the basic type entry, _msg_ is the message to print to screen
+    def _RaiseMsg_(self, _type_, _msg_):
+
+        _MSG_ 
+    # Start Up Object
+    def _StartUp_(self):
+        
+        self._HandleCommands_()
+def Main():
+    
+    main = MainOps()
+    main._StartUp_()
+
+Main()
